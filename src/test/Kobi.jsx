dd3139c852531ad5de555261c51e4b7b7140f657
kobikru <75983501+kobikru@users.noreply.github.com>
2024-05-05 17:29:21 +0300

    loader

--- src/test/Kobi.jsx
+++ src/test/Kobi.jsx
@@ -1,12 +1,23 @@
-import BtnBack from "../components/BtnBack";
+import { RotatingLines } from "react-loader-spinner";
 import X from "../components/X";
 import O from "../components/O";
-import Menu from "../pages/Menu";
 
 export default function Kobi() {
   return (
     <div>
       <h2>Welcome to Kobi's Component</h2>
+      <RotatingLines
+        strokeColor="yellow"
+        visible={true}
+        height="96"
+        width="96"
+        color="grey"
+        strokeWidth="8"
+        animationDuration="3"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
       <O />
       <X />
     </div>
--- src/test/Ronini.jsx
+++ src/test/Ronini.jsx
@@ -1,5 +1,5 @@
 import Button from "../components/Button";
-// import YellowElement from "../components/Yellow_Element";
+import YellowElement from "../components/Yellow_Element";
 
 export default function Ronini() {
   return (
