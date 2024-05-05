8166abf25a3cd7bff881ddb33b0ade4469f08674
kobikru <75983501+kobikru@users.noreply.github.com>
2024-05-05 15:44:42 +0300

    X+O

---
+++ src/components/O/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+export default function X() {
+  return (
+    <div className={styles.body}>
+      <img src="../../../public/O.svg" />
+    </div>
+  );
+}
