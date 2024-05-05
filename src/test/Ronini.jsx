5cc66fc9bf374b0332753d38480a84db3a139035
roni sa <roni1900@gmail.com>
2024-05-05 18:50:33 +0300

    Game Board

--- src/test/Ronini.jsx
+++ src/test/Ronini.jsx
@@ -1,11 +1,13 @@
 import Button from "../components/Button";
+import Game_board from "../components/GameBoard";
 import YellowElement from "../components/Yellow_Element";
 
 export default function Ronini() {
   return (
     <div>
        <h2>Welcome to Ronini's Component</h2>
-       <YellowElement/> 
+       {/* <YellowElement/>  */}
+       <Game_board/>
        
 
 </div>
