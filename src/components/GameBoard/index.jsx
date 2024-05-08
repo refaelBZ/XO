b7b58e86a8c9e843cbbdb21babc7e739c64bd4d4
kobikru <75983501+kobikru@users.noreply.github.com>
2024-05-08 12:08:57 +0300

    bla

--- src/components/GameBoard/index.jsx
+++ src/components/GameBoard/index.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from 'react'
-import styles from './style.module.scss'
-import Blue_Square from '../Blue_square/index'
-import WhiteBoard from '../WhiteBoard';
+import { useState } from "react";
+import styles from "./style.module.scss";
+import Blue_Square from "../Blue_square/index";
+import WhiteBoard from "../WhiteBoard";
 
 export default function Game_board() {
+  const list = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
 
-    const list = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
-    ];
+  const [board, setBoard] = useState(list);
+  const [player, setPlayer] = useState("X");
 
-    const [board ,setBoard]= useState(list)
-    const [player, setPlayer] = useState("X")
-
-    const handleClick = (rowIndex , squareIndex) => {
-        const newBoard = [...board]
-        if (board[rowIndex][squareIndex] !== " ") return null
-        newBoard[rowIndex][squareIndex] = player
-        setBoard(newBoard)
-        setPlayer(player==="X"?"O":"X")
-        // console.log(player);
-
-    };
+  const handleClick = (rowIndex, squareIndex) => {
+    const newBoard = [...board];
+    if (board[rowIndex][squareIndex] !== " ") return null;
+    newBoard[rowIndex][squareIndex] = player;
+    setBoard(newBoard);
+    setPlayer(player === "X" ? "O" : "X");
     // console.log(player);
-    return (
-        <div >
-            <WhiteBoard>
-
-                {board.map((r, rowIndex) => (
-                    <div key={rowIndex} className={styles.row}>
-                        {r.map((s, squareIndex) => (
-                            <Blue_Square
-                                key={squareIndex}
-                                value={s}
-                                onClick={()=>handleClick(rowIndex , squareIndex)}
-                            />
-                        ))}
-                    </div>
-                ))}
-            </WhiteBoard>
-        </div>
-    );
+  };
+  // console.log(player);
+  return (
+    <div>
+      <WhiteBoard>
+        {board.map((r, rowIndex) => (
+          <div key={rowIndex} className={styles.row}>
+            {r.map((s, squareIndex) => (
+              <Blue_Square
+                key={squareIndex}
+                value={s}
+                onClick={() => handleClick(rowIndex, squareIndex)}
+              />
+            ))}
+          </div>
+        ))}
+      </WhiteBoard>
+    </div>
+  );
 }
